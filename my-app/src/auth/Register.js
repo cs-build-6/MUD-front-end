7d7a2f5e8b48b9cdf3c20e2f99af3e35eed63e62
Pablo Ogata <pabloogata@gmail.com>
2019-10-23 12:35:54 -0400

    register/login

---
+++ my-app/src/auth/Register.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+function Registration(props) {
+  const [userObj, setUser] = useState({});
+
+  const handleChange = e => {
+    setUser({
+      ...userObj,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleLogin = e => {
+    e.preventDefault();
+    axios
+    // need to swap this with our server later
+      .post(`https://lambda-mud-test.herokuapp.com/api/registration/`, userObj)
+      .then(res => {
+        localStorage.setItem("key", res.data.key);
+        props.history.push("/protected");
+      })
+      .catch(error => {
+        console.log(error.message);
+      });
+  };
+
+  return (
+    <div className="Login-Page">
+      <form className="login-form" onSubmit={handleLogin}>
+        <h1 className="Login-Title">Register</h1>
+        <input
+          type="text"
+          placeholder="username"
+          name="username"
+          value={userObj.username}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          name="password1"
+          value={userObj.password1}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="confirm password"
+          name="password2"
+          value={userObj.password2}
+          onChange={handleChange}
+        />
+        <button className="registerBtn">Register</button>
+      </form>
+    </div>
+  );
+}
+
+export default Registration;
